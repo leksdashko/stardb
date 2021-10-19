6821f4ddc2e7c7105a668b1385c3261d765d08d1
Alexander Dashko <leksdashko@gmail.com>
2021-10-19 11:56:26 +0300

    hoc composition

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -50,15 +50,9 @@ export default class App extends Component {
         <PlanetDetails itemId={3}/>
         <StarshipDetails itemId={5}/>
 
-        <PersonList>
-          { ({name}) => <span>{name}</span> }
-        </PersonList>
-        <PlanetList>
-          { ({name}) => <span>{name}</span> }
-        </PlanetList>
-        <StarshipList>
-          { ({name}) => <span>{name}</span> }
-        </StarshipList>
+        <PersonList />
+        <PlanetList />
+        <StarshipList />
 
       </div>
       </ErrorBoundary>
